30d9c081c74c331e27fb66d2f999efbfd858fc70
p1an7 <chervony.ivan@gmail.com>
2024-10-10 10:45:14 +0300

    translate xr_gpio.py

--- Python/xr_gpio.py
+++ Python/xr_gpio.py
@@ -18,33 +18,33 @@
 
 import RPi.GPIO as GPIO
 
-# 设置引脚模式
+# Set pin mode
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# 蜂鸣器引脚
+# Buzzer pin
 BUZZER = 10
 
-# 设置电机引脚
-ENA = 13  	# //L298使能A
-ENB = 20  	# //L298使能B
-IN1 = 16  	# //电机接口1
-IN2 = 19  	# //电机接口2
-IN3 = 26  	# //电机接口3
-IN4 = 21  	# //电机接口4
+# Set the motor pin
+ENA = 13  	# //L298 enable A
+ENB = 20  	# //L298 enable B
+IN1 = 16  	# //Motor interface 1
+IN2 = 19  	# //Motor interface 2
+IN3 = 26  	# //Motor interface 3
+IN4 = 21  	# //Motor interface 4
 
-# 设置超声波引脚
-ECHO = 4  	# 超声波接收脚位
-TRIG = 17  	# 超声波发射脚位
+# Setting up the Ultrasonic Pin
+ECHO = 4  	# Ultrasonic receiving pin
+TRIG = 17  	# Ultrasonic transmitting pin
 
-# 设置红外引脚
-IR_R = 18  	# 小车右侧巡线红外
-IR_L = 27  	# 小车左侧巡线红外
-IR_M = 22  	# 小车中间避障红外
-IRF_R = 25  # 小车跟随右侧红外
-IRF_L = 1  # 小车跟随左侧红外
+# Setting up the infrared pins
+IR_R = 18  	# Infrared line patrol on the right side of the car
+IR_L = 27  	# The left side of the car patrols the infrared
+IR_M = 22  	# Infrared obstacle avoidance in the middle of the car
+IRF_R = 25  # The car follows the right infrared
+IRF_L = 1  # The car follows the left infrared
 
-# 引脚初始化使能
+# Pin initialization enable
 GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
@@ -64,34 +64,34 @@ GPIO.setup(IR_L, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(IR_M, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(IRF_R, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(IRF_L, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# 超声波脚初始化使能
-GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)  			# 超声波模块发射端管脚设置trig
-GPIO.setup(ECHO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  	# 超声波模块接收端管脚设置echo
-# 蜂鸣器脚初始化使能
-GPIO.setup(BUZZER, GPIO.OUT, initial=GPIO.LOW)			# 蜂鸣器设置为低电平
+# Infrared pin initialization enable
+GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)  			# Ultrasonic module transmitter pin settings trig
+GPIO.setup(ECHO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  	# Ultrasonic module receiving end pin setting echo
+# Buzzer pin initialization enable
+GPIO.setup(BUZZER, GPIO.OUT, initial=GPIO.LOW)			# The buzzer is set to low level
 
 
 def digital_write(gpio, status):
 	"""
-	设置gpio端口为电平
-	参数：gpio为设置的端口，status为状态值只能为True(高电平)，False(低电平)
+	Set the gpio port to level
+	Parameters: gpio is the port to be set, status is the status value which can only be True (high level) or False (low level)
 	"""
 	GPIO.output(gpio, status)
 
 def digital_read(gpio):
 	"""
-	读取gpio端口的电平
+	Read the level of the gpio port
 	"""
 	return GPIO.input(gpio)
 
 def ena_pwm(pwm):
 	"""
-	设置电机调速端口ena的pwm
+		Set the PWM of the motor speed control port ena
 	"""
 	ENA_pwm.ChangeDutyCycle(pwm)
 
 def enb_pwm(pwm):
 	"""
-	设置电机调速端口enb的pwm
+	Set the pwm of the motor speed control port enb
 	"""
 	ENB_pwm.ChangeDutyCycle(pwm)
